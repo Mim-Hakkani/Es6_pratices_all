8d2ce394439be6c7cfca7ad1b4e3079a1b6e57ca
Hakkani Mim <mimhakkani4g@gmail.com>
2021-08-26 11:02:59 +0600

    advanced level one is done

--- exam.js
+++ exam.js
@@ -221,3 +221,27 @@ Forach:
         // const finalValue =  defaultParameter(4,6); //third parameter of default
         // console.log(finalValue);
 
+
+/*
+১১. একটা nested অবজেক্ট ডিক্লেয়ার করো (অর্থাৎ একটা অবজেক্ট এর প্রপার্টি এর মধ্যেও যে অবজেক্ট থাকতে পারে। আবার সেই অবজেক্ট এর প্রপার্টি এর মধ্যেও সে অবজেক্ট থাকতে পারে। সেই রকম একটা অবজেক্ট ডিক্লেয়ার করো। এবং যেকোন একটা প্রপার্টি এর মান একটা array হবে। জাস্ট এমন একটা অবজেক্ট )
+*/
+
+  const phones = {
+            name:'Lava',
+            price:6200,
+            color: {
+                nice:'red',
+                awesome : 'silver',
+                beautiful:'white',
+                bad:{
+                    name1:'golam Hakkni mim',
+                    name2:'Golam kibria Jim',
+                    name3:'Golam Rabbani',
+                    name4:[4,6,7,8,9]
+                  }
+              }
+            }
+
+    const{name4} = phones.color.bad;
+    const[a,b,c,d,e]=name4
+    console.log(e);
